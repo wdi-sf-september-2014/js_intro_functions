f339453cc84bdd3c7969b8ed0d4bbd8fc5fd4a26
rragno <robertragno@gmail.com>
2014-10-01 10:57:32 -0700

    Added my assignment

--- robert_ragno/app.js
+++ robert_ragno/app.js
@@ -1,1 +1,16 @@
-//Your code here
+var myAge = function(birth) {
+    var time = new Date();
+    var year = time.getFullYear();
+    if (year - birth > 0) {
+    return console.log("You are  either " + (year - birth) + " or " + (year - birth - 1) + " years old.");
+    }
+    else {
+    return console.log("You haven't been conceived yet.");
+    }
+};
+
+myAge(1986);
+
+myAge(1990);
+
+myAge(2016);
