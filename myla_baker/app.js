5e01e54fe4961092c18826d3eb941897990f0275
Myla Baker <myla.baker@gmail.com>
2014-10-01 11:05:20 -0700

    added my lab exercise

--- myla_baker/app.js
+++ myla_baker/app.js
@@ -1,1 +1,9 @@
-//Your code here
+function calculateAge(byear, cyear) {
+    return "You are either "+ (cyear - byear) + " or " + ((cyear- byear)-1);
+}
+
+calculateAge(1988, 2014);
+
+calculateAge(1955, 2014);
+
+calculateAge(1949, 2014);
