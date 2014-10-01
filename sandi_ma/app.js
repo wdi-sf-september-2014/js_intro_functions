5e2dd4a651cfb30f1d74ed80241be3aba081a908
Sandi <sandima.acro@gmail.com>
2014-10-01 14:35:54 -0700

    Sandi's code

--- sandi_ma/app.js
+++ sandi_ma/app.js
@@ -1,1 +1,11 @@
+var calcAge = function (birthYr, curYr){
+    birthYr = prompt ("What is your birth year?");
+    curYr = prompt ("What is the current year?");
+    alert ("Your current age is either: " + (curYr-birthYr-1) + " or "+ (curYr-birthYr));
+   
+}
+
+calcAge();
+
+
 //Your code here
